d379c4f4a210f3dbbc8558f4ecddf5d83f144b9c
M.U <rustamovmuhammadumar2004@gmail.com>
2026-09-02 18:35:40 +0500

    fix: route help page separately from home

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-// @ts-ignore: Allow side-effect CSS import without type declarations
 import "../css/app.css";
 import { Route, Switch, useLocation } from "react-router-dom";
 import HomePage from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
+import { HelpPage } from "./screens/helpPage";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar  from "./components/headers/OtherNavbar";
 import Footer  from "./components/footer";
@@ -25,7 +25,10 @@ function App() {
         <Route path="/member-page">
           <UserPage/>
         </Route>
-        <Route path="/">
+        <Route path="/help">
+          <HelpPage/>
+        </Route>
+        <Route exact path="/">
           <HomePage/>
         </Route>
       </Switch>
