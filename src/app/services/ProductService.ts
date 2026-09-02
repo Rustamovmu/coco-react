6e93e7d0fa621a24f6462672882de1aa32dcf2eb
M.U <rustamovmuhammadumar2004@gmail.com>
2026-09-02 17:09:35 +0500

    feat: creating ProductService

--- src/app/services/ProductService.ts
+++ src/app/services/ProductService.ts
@@ -1,1 +1,37 @@
-export {};
+import axios from "axios";
+import { serverApi } from "../../lib/config";
+import { Product, ProductInquiry } from "../../lib/types/product";
+
+class ProductService {
+  private readonly path: string;
+
+  constructor() {
+    this.path = `${serverApi}/product`;
+  }
+
+  public async getProducts(input: ProductInquiry): Promise<Product[]> {
+    const params = new URLSearchParams({
+      page: String(input.page),
+      limit: String(input.limit),
+    });
+
+    if (input.order) params.set("order", input.order);
+    if (input.productCollection) {
+      params.set("productCollection", input.productCollection);
+    }
+    if (input.productStatus) params.set("productStatus", input.productStatus);
+    if (input.size) params.set("size", input.size);
+    if (input.color) params.set("color", input.color);
+    if (input.search) params.set("search", input.search);
+
+    const result = await axios.get<Product[]>(`${this.path}/all?${params.toString()}`);
+    return result.data;
+  }
+
+  public async getProduct(productId: string): Promise<Product> {
+    const result = await axios.get<Product>(`${this.path}/${productId}`);
+    return result.data;
+  }
+}
+
+export default ProductService;
