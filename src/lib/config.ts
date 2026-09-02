d49ceb2ad32b1a7403d95a3cd383e630503bdec6
M.U <rustamovmuhammadumar2004@gmail.com>
2026-09-02 16:48:47 +0500

    feat: implementing config file and its logic

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,9 @@
-export {};
+export const serverApi: string = `${process.env.REACT_APP_API_URL}`;
+
+export const Messages = {
+    error1: "Something went wrong!",
+    error2: "Please login first!",
+    error3: "Please fulfil all the required fields!",
+    error4: "Message is empty!",
+    error5: "Only images in jpeg, jpg, png formats are allowed!",
+};
