5d2bb8a1783af61703bbc43d6a8a45ca77a109fd
M.U <rustamovmuhammadumar2004@gmail.com>
2026-09-02 17:08:09 +0500

    feat: creating OrderService

--- src/app/services/OrderService.ts
+++ src/app/services/OrderService.ts
@@ -1,1 +1,58 @@
-export {};
+import axios from "axios";
+import { serverApi } from "../../lib/config";
+import {
+  Order,
+  OrderInquiry,
+  OrderItemInput,
+  OrderUpdateInput,
+} from "../../lib/types/order";
+import { CartItem } from "../../lib/types/search";
+
+class OrderService {
+  private readonly path: string;
+
+  constructor() {
+    this.path = `${serverApi}/order`;
+  }
+
+  public async createOrder(cartItems: CartItem[]): Promise<Order> {
+    const orderItems: OrderItemInput[] = cartItems.map((cartItem) => ({
+      quantity: cartItem.quantity,
+      unitPrice: cartItem.unitPrice,
+      productId: cartItem.productId,
+      selectedSize: cartItem.selectedSize,
+      selectedColor: cartItem.selectedColor,
+    }));
+
+    const result = await axios.post<Order>(`${this.path}/create`, orderItems, {
+      withCredentials: true,
+    });
+
+    return result.data;
+  }
+
+  public async getMyOrders(input: OrderInquiry): Promise<Order[]> {
+    const params = new URLSearchParams({
+      page: String(input.page),
+      limit: String(input.limit),
+    });
+
+    if (input.orderStatus) params.set("orderStatus", input.orderStatus);
+
+    const result = await axios.get<Order[]>(`${this.path}/all?${params.toString()}`, {
+      withCredentials: true,
+    });
+
+    return result.data;
+  }
+
+  public async updateOrder(input: OrderUpdateInput): Promise<Order> {
+    const result = await axios.post<Order>(`${this.path}/update`, input, {
+      withCredentials: true,
+    });
+
+    return result.data;
+  }
+}
+
+export default OrderService;
